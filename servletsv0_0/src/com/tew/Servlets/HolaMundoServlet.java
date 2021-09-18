48cc7a880bf33f2f4554b5b2608faf00670ef4e0
jorge <jorge@DESKTOP-199VTVA>
2021-09-18 17:58:37 +0200

    “TEW-2022-312-2.2-Servelts con sesiones.”

--- servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package com.tew.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,7 +30,7 @@ public class HolaMundoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//Primera version
+		//Inicial version
 		/*	 response.setCharacterEncoding("UTF-8");
 			 response.setContentType("text/html");
 			 PrintWriter out = response.getWriter();
@@ -40,7 +41,8 @@ public class HolaMundoServlet extends HttpServlet {
 			 out.println("</BODY></HTML>");
 			}
 	*/
-		 String nombre = (String) request.getParameter("NombreUsuario");
+		//Primera version
+		 /*String nombre = (String) request.getParameter("NombreUsuario");
 		 response.setCharacterEncoding("UTF-8");
 		 response.setContentType("text/html");
 		 PrintWriter out = response.getWriter();
@@ -53,6 +55,38 @@ public class HolaMundoServlet extends HttpServlet {
 		 }
 		 out.println("Bienvenido a mi primera página Web!");
 		 out.println("</BODY></HTML>");
+		 */
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String nombre = (String) request.getParameter("NombreUsuario");
+		@SuppressWarnings("unchecked")
+		Vector <String> listado = (Vector <String>)request.getSession().getAttribute("listado");
+			if (listado == null){
+				 listado = new Vector<String>();
+
+			}
+			
+			
+			if ( nombre != null ){
+				 out.println("<br>Hola "+nombre+"<br>");
+				 listado.addElement(nombre);
+				}
+			request.getSession().setAttribute("listado",listado);
+			out.println("<br>");
+			out.println("Contigo, hoy me han visitado:<br>");
+			for ( int i = 0 ; i < listado.size() ; i++ ){
+				out.println("<br>"+(String)listado.elementAt(i));
+				
+			}
+			System.out.println("<a href=\"index.html\">volver</a>");
+			Integer contador=(Integer)request.getServletContext().getAttribute("contador");
+			
+			
+			
+		
 	}
 
 	/**
